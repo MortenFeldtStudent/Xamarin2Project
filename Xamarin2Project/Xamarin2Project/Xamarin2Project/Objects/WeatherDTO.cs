a5cb721a8e31dcd77ab25be79c9b9ce2aafa5919
MortenFeldtStudent <cph-mf227@cphbusiness.dk>
2019-09-24 11:28:00 +0200

    WeatherPage OK

---
+++ Xamarin2Project/Xamarin2Project/Xamarin2Project/Objects/WeatherDTO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin2Project.Objects
+{
+    public class WeatherDTO
+    {
+        public ulong id { get; set; }
+        public double min_temp { get; set; }
+        public double max_temp { get; set; }
+        public double the_temp { get; set; }
+        public double wind_speed { get; set; }
+        public string weather_state_name { get; set; }
+        public string weather_state_abbr { get; set; }
+        public string wind_direction_compass { get; set; }
+        public string applicable_date { get; set; }
+        public string image_url { get; set; }
+        public int maxTempRaw { get; set; }
+        public string dayOfWeek { get; set; }
+
+        public void setImageUrl()
+        {
+            image_url = "https://www.metaweather.com/static/img/weather/ico/" + weather_state_abbr + ".ico";
+        }
+
+        public void setTempToInt()
+        {
+            maxTempRaw = Convert.ToInt32(max_temp);
+        }
+
+        public void setDayOfWeek()
+        {
+            string[] arr = applicable_date.Split('-');
+            int year = Convert.ToInt32(arr[0]);
+            int month = Convert.ToInt32(arr[1]);
+            int day = Convert.ToInt32(arr[2]);
+            DateTime dateValue = new DateTime(year, month, day);
+            dayOfWeek = dateValue.ToString("ddd");
+        }
+    }
+}
--- Xamarin2Project/Xamarin2Project/Xamarin2Project/WeatherPage.xaml.cs
+++ Xamarin2Project/Xamarin2Project/Xamarin2Project/WeatherPage.xaml.cs
@@ -3,18 +3,82 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin2Project.Objects;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Net.Http;
+using Newtonsoft.Json;
+
 
 namespace Xamarin2Project
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WeatherPage : ContentPage
     {
+        //public string citySearched = "yo";
+        private List<WeatherDTO> weatherDTOList = new List<WeatherDTO>();
+        //private string search = "copenhagen";
+        private HttpClient client = new HttpClient();
+        private string Url = "https://www.mortenfeldtstudent.dk/WeatherApiProject/api/weather/5days/";
+        //private string weatherXaml = "";
         public WeatherPage()
         {
             InitializeComponent();
+        }
+
+        private void OnSearchClicked(object sender, EventArgs e)
+        {
+            citySearched.Text = entryCity.Text;
+            GetDataFromAPIAsync(entryCity.Text);
+            entryCity.Text = "";
+            entryCity.Focus();
+        }
+        private async void GetDataFromAPIAsync(string city)
+        {
+            var response = await client.GetStringAsync(Url + city);
+
+            var weatherList = JsonConvert.DeserializeObject<List<WeatherDTO>>(response);
+
+            for (var i = 0; i < weatherList.Count; i++)
+            {
+                weatherList[i].setImageUrl();
+                weatherList[i].setTempToInt();
+                weatherList[i].setDayOfWeek();
+            }
+
+            weatherView.ItemsSource = weatherList;
+        }
+
+        //string myDataTemplate = new DataTemplate(() =>
+        //{
+        //    var cell = new ViewCell();
+        //    var grid = new Grid();
+
+        //    foreach (var record in myRecords)
+        //    {
+        //        grid.RowDefinitions.Add(new RowDefinition());
+        //    }
+
+        //    foreach (var field in myFields)
+        //    {
+        //        grid.ColumnDefinitions.Add(new ColumnDefinition());
+        //    }
+
+        //    /*
+        //     * 
+        //     * Populate grid here...
+        //     * 
+        //     */
+
+        //    cell.View = grid;
+        //    return cell;
+        //});
+
+        private string CapitalLetter(string str)
+        {
+            string uppercase = str.ToUpper();
+            string lowercase = str.ToLower();
+            return uppercase.Substring(0, 1) + lowercase.Substring(1);
         }
     }
 }
